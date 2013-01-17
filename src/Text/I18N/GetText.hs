bd8d7f7281c23dd49df604c4699ff0888f632f4f
Vasyl Pasternak <vasyl.pasternak@gmail.com>
2013-01-17 11:44:12 +0200

    version 0.1.20

---
+++ src/Text/I18N/GetText.hs
@@ -0,0 +1,186 @@
+-- | This library provides basic internationalization capabilities
+
+module Text.I18N.GetText (
+                          getText,
+                          nGetText,
+                          dGetText,
+                          dnGetText,
+                          dcGetText,
+                          dcnGetText,
+                          bindTextDomain,
+                          textDomain
+                         ) where
+
+import Foreign.C.Types
+import Foreign.C.String
+import Foreign.C.Error
+import Foreign.Ptr
+import Data.Maybe (fromMaybe)
+import System.Locale.SetLocale
+
+
+foreign import ccall unsafe "libintl.h gettext" c_gettext
+    :: CString -> IO CString
+
+foreign import ccall unsafe "libintl.h dgettext" c_dgettext
+    :: CString -> CString -> IO CString
+
+foreign import ccall unsafe "libintl.h dcgettext" c_dcgettext 
+    :: CString -> CString -> CInt -> IO CString
+
+foreign import ccall unsafe "libintl.h ngettext" c_ngettext
+    :: CString -> CString -> CULong -> IO CString
+
+foreign import ccall unsafe "libintl.h dngettext" c_dngettext
+    :: CString -> CString -> CString -> CULong -> IO CString
+
+foreign import ccall unsafe "libintl.h dcngettext" c_dcngettext
+    :: CString -> CString -> CString -> CULong -> CInt -> IO CString
+
+foreign import ccall unsafe "libintl.h bindtextdomain" c_bindtextdomain
+    :: CString -> CString -> IO CString
+
+foreign import ccall unsafe "libintl.h textdomain" c_textdomain
+    :: CString -> IO CString
+
+fromCString :: CString -> IO (Maybe String)
+fromCString x | x == nullPtr = return Nothing
+              | otherwise = peekCString x >>= return . Just
+
+fromCStringError :: String -> CString -> IO String
+fromCStringError err x | x == nullPtr = throwErrno err
+                       | otherwise = peekCString x
+
+fromCStringDefault :: String -> CString -> IO String
+fromCStringDefault d x = fromCString x >>= \r -> return (fromMaybe d r)
+
+fromCStringPluralDefault :: Num a => String -> String -> a -> CString -> IO String
+fromCStringPluralDefault def def_plural n s
+    | n == 1 = fromCStringDefault def s
+    | otherwise = fromCStringDefault def_plural s
+    
+
+withCStringMaybe :: Maybe String -> (CString -> IO a) -> IO a
+withCStringMaybe Nothing f = f nullPtr
+withCStringMaybe (Just str) f = withCString str f
+
+-- |getText wraps GNU gettext function. It returns translated string for the
+-- input messages. If translated string not found the input string will be
+-- returned.
+--
+-- The most common usage of this function is to declare function __:
+-- 
+-- > __ = unsafePerformIO . getText
+--
+-- and wrap all text strings into this function, e.g.
+-- 
+-- > printHello = putStrLn (__ "Hello")
+-- 
+getText :: String -> IO String
+getText s = 
+    withCString s $ \s' -> 
+        c_gettext s' >>= fromCStringDefault s
+
+-- |dGetText wraps GNU dgettext function. It works similar to 'getText'
+-- but also could take domain name.
+--
+dGetText :: Maybe String        -- ^ domain name, if 'Nothing' ---
+                                -- default domain will be used
+         -> String              -- ^ message id
+         -> IO String           -- ^ return value
+dGetText domainname msgid = 
+    withCStringMaybe domainname $ \dn' ->
+        withCString msgid $ \msg' ->
+            c_dgettext dn' msg' >>= fromCStringDefault msgid
+
+-- |dcGetText wraps GNU dcgettext function. It works similar to 'dGetText'
+-- but also takes category id
+dcGetText :: Maybe String       -- ^ domain name, if 'Nothing' ---
+                                -- default domain will be used
+          -> Category           -- ^ locale facet
+          -> String             -- ^ message id
+          -> IO String          -- ^ return value
+dcGetText domainname cat msgid = 
+    withCStringMaybe domainname $ \dn' ->
+        withCString msgid $ \msg' ->
+            c_dcgettext dn' msg' (categoryToCInt cat) >>= 
+            fromCStringDefault msgid
+                                
+-- |nGetText wraps GNU ngettext function. It translates text string in the
+-- user's native language, by lookilng up the approppiate plural form of the
+-- message.
+--
+nGetText :: String              -- ^ msgid in singular form
+         -> String              -- ^ msgid in plural form
+         -> Integer             -- ^ number, used to choose appropriate form
+         -> IO String           -- ^ result string, by default if number is 1 than
+                                -- singular form of msgid is returned, otherwise ---
+                                -- plural 
+nGetText msgid msgid_plural n =
+    withCString msgid $ \msgid' ->
+        withCString msgid_plural $ \msgid_plural' ->
+            c_ngettext msgid' msgid_plural' (fromInteger n) >>=
+            fromCStringPluralDefault msgid msgid_plural n
+
+-- |dnGetText wraps GNU dngettext function. It works similar to 'nGetText' but
+-- also takes domain name
+-- 
+dnGetText :: Maybe String       -- ^ domain name, if 'Nothing' ---
+                                -- default domain will be used
+          -> String             -- ^ msgid in singular form
+          -> String             -- ^ msgid in plural form
+          -> Integer            -- ^ number, used to choose appropriate form
+          -> IO String          -- ^ result string, by default if number is 1 than
+                                -- singular form of msgid is returned, otherwise ---
+                                -- plural 
+dnGetText domainname msgid msgid_plural n =
+    withCStringMaybe domainname $ \dn' ->
+        withCString msgid $ \msgid' ->
+            withCString msgid_plural $ \msgid_plural' ->
+                c_dngettext dn' msgid' msgid_plural' (fromInteger n) >>=
+                fromCStringPluralDefault msgid msgid_plural n
+
+-- |dcnGetText wraps GNU dcngettext function. It works similar to 'dnGetText' but
+-- also takes category id
+-- 
+dcnGetText :: Maybe String      -- ^ domain name, if 'Nothing' ---
+                                -- default domain will be used
+          -> Category           -- ^ locale facet
+          -> String             -- ^ msgid in singular form
+          -> String             -- ^ msgid in plural form
+          -> Integer            -- ^ number, used to choose appropriate form
+          -> IO String          -- ^ result string, by default if number is 1 than
+                                -- singular form of msgid is returned, otherwise ---
+                                -- plural 
+dcnGetText domainname cat msgid msgid_plural n =
+    withCStringMaybe domainname $ \dn' ->
+        withCString msgid $ \msgid' ->
+            withCString msgid_plural $ \msgid_plural' ->
+                c_dcngettext dn' msgid' msgid_plural' 
+                             (fromInteger n) (categoryToCInt cat) >>=
+                fromCStringPluralDefault msgid msgid_plural n
+
+-- |bindTextDomain sets the base directory of the hierarchy
+-- containing message catalogs for a given message domain.
+--
+-- Throws 'IOError' if fails
+--
+bindTextDomain :: String        -- ^ domain name
+               -> Maybe String  -- ^ path to the locale folder or 'Nothing' to return
+                                -- base directory for domain
+               -> IO String     -- ^ return value
+bindTextDomain domainname dirname = 
+  withCString domainname $ \domain -> 
+      withCStringMaybe dirname $ \dir ->
+          c_bindtextdomain domain dir >>= fromCStringError "bindTextDomain fails"
+
+-- |textDomain sets domain for future 'getText' call
+--
+-- Throws 'IOError' if fails
+-- 
+textDomain :: Maybe String      -- ^ domain name, if 'Nothing' than returns
+                                -- current domain name
+           -> IO String         -- ^ return value
+textDomain domainname = 
+    withCStringMaybe domainname $ \domain ->
+        c_textdomain domain >>= fromCStringError "textDomain fails"
---
+++ src/hgettext.hs
@@ -0,0 +1,97 @@
+
+import qualified Language.Haskell.Exts as H 
+
+import System.Environment
+import System.Console.GetOpt
+
+import Data.Generics.PlateData
+
+import Distribution.Simple.PreProcess.Unlit
+
+import Data.List
+import Data.Char
+import System.FilePath
+
+import Paths_hgettext (version)
+import Data.Version (showVersion)
+
+data Options = Options {
+      outputFile :: String,
+      keyword :: String,
+      printVersion :: Bool
+    } deriving Show
+
+options :: [OptDescr (Options->Options)]
+options = 
+    [
+     Option ['o'] ["output"] 
+                (ReqArg (\o opts -> opts {outputFile = o}) "FILE") 
+                "write output to specified file",
+     Option ['d'] ["default-domain"] 
+            (ReqArg (\d opts -> opts {outputFile = d ++ ".po"}) "NAME")
+            "use NAME.po instead of messages.po",
+     Option ['k'] ["keyword"] 
+            (ReqArg (\d opts -> opts {keyword = d}) "WORD")
+            "function name, in which wrapped searched words",
+     Option [] ["version"]
+            (NoArg (\opts -> opts {printVersion = True}))
+            "print version of hgettexts"
+    ]
+
+
+defaultOptions = Options "messages.po" "__" False
+
+parseArgs :: [String] -> IO (Options, [String])
+parseArgs args = 
+    case getOpt Permute options args of
+      (o, n, []) -> return (foldl (flip id) defaultOptions o, n)
+      (_, _, errs) -> ioError (userError (concat errs ++ usageInfo header options))
+    where header = "Usage: hgettext [OPTION] [INPUTFILE] ..."
+
+
+toTranslate :: String -> H.ParseResult H.Module -> [(Int, String)]
+toTranslate f (H.ParseOk z) = nub [ (0, s) | H.App (H.Var (H.UnQual (H.Ident x))) (H.Lit (H.String s)) <- universeBi z, x == f]
+toTranslate _ _ = []
+
+-- Create list of messages from a single file
+formatMessages :: String -> [(Int, String)] -> String
+formatMessages src l = concat $ map potEntry l
+    where potEntry (l, s) = unlines [
+                             "#: " ++ src ++ ":" ++ (show l),
+                             "msgid " ++ (show s),
+                             "msgstr \"\"",
+                             ""
+                            ]
+
+
+writePOTFile :: [String] -> String
+writePOTFile l = concat $ [potHeader] ++ l
+    where potHeader = unlines ["# Translation file",
+                               "",
+                               "msgid \"\"",
+                               "msgstr \"\"",
+                               "",
+                               "\"Project-Id-Version: PACKAGE VERSION\\n\"",
+                               "\"Report-Msgid-Bugs-To: \\n\"",
+                               "\"POT-Creation-Date: 2009-01-13 06:05-0800\\n\"",
+                               "\"PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\\n\"",
+                               "\"Last-Translator: FULL NAME <EMAIL@ADDRESS>\\n\"",
+                               "\"Language-Team: LANGUAGE <LL@li.org>\\n\"",
+                               "\"MIME-Version: 1.0\\n\"",
+                               "\"Content-Type: text/plain; charset=UTF-8\\n\"",
+                               "\"Content-Transfer-Encoding: 8bit\\n\"",
+                               ""]
+
+process :: Options -> [String] -> IO ()
+process Options{printVersion = True} _ = 
+    putStrLn $ "hgettext, version " ++ (showVersion version)
+
+process opts fl = do
+  t <- mapM read' fl
+  writeFile (outputFile opts) $ writePOTFile $ map (\(n,c) -> formatMessages n $ toTranslate (keyword opts) c) t
+    where read' "-" = getContents >>= \c -> return ("-", H.parseFileContents c)
+          read' f = H.parseFile f >>= \m -> return (f, m)
+
+main = 
+    getArgs >>= parseArgs >>= uncurry process
+
